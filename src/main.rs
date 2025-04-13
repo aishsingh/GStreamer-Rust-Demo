63c1b08cffb82704f57a23e3ba3ce330bb6e2a1d
Aishwarya Singh <aishsgh@gmail.com>
2025-04-14 04:56:29 +1000

    split pipeline into input and output

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,37 @@ use gstreamer as gst;
 use gstreamer::prelude::*;
 use anyhow::Result;
 
-// My webcam device on linux which will be used as the video source
+// The webcam device on my linux system which will be used as the video source
 const WEBCAM: &str = "/dev/video2";
 
 fn main() -> Result<()> {
     
     println!("GStreamer Rust DEMO - Aishwarya Singh");
 
-
     // Initialize GStreamer
+    println!("Initializing GStreamer...");
     gst::init()?;
 
-    // Create a GStreamer pipeline to display webcam feed
-    let pipeline = gst::parse_launch(
-        format!("v4l2src device={} ! videoconvert ! autovideosink", WEBCAM).as_str()
-    )?;
 
-    // Start playing the pipeline
-    pipeline.set_state(gst::State::Playing)?;
+    // GStreamer input pipeline to access webcam frames from the source
+    let input_pipeline = gst::parse_launch(&format!("v4l2src device={} ! videoconvert ! video/x-raw,format=BGR ! appsink name=sink"), WEBCAM).unwrap();
+
+    let appsink = input_pipeline
+    .by_name("sink").unwrap()
+    .downcast::<gst_app::AppSink>().unwrap();
+
+
+    // GStreamer output pipeline to push webcam frames to the sink
+    let output_pipeline = gst::parse_launch("appsrc name=source ! videoconvert ! autovideosink").unwrap();
+
+    let appsrc = output_pipeline
+    .by_name("source").unwrap()
+    .downcast::<gst_app::AppSrc>().unwrap();
+
+
+    // Start playing the pipelines
+    input_pipeline.set_state(gst::State::Playing)?;
+    output_pipeline.set_state(gst::State::Playing)?;
 
     // Wait until EOS (end-of-stream) or error
     let bus = pipeline.bus().unwrap();
