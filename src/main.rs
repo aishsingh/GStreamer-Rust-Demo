425d844520c6833f5e92611f94509ea6ac038dd5
Aishwarya Singh <aishsgh@gmail.com>
2025-04-14 05:03:29 +1000

    Implemented callbacks

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,12 @@ const WEBCAM: &str = "/dev/video2";
 fn main() -> Result<()> {
     
     println!("GStreamer Rust DEMO - Aishwarya Singh");
-
-    // Initialize GStreamer
     println!("Initializing GStreamer...");
     gst::init()?;
 
 
     // GStreamer input pipeline to access webcam frames from the source
-    let input_pipeline = gst::parse_launch(&format!("v4l2src device={} ! videoconvert ! video/x-raw,format=BGR ! appsink name=sink"), WEBCAM).unwrap();
+    let input_pipeline = gst::parse_launch(&format!("v4l2src device={} ! videoconvert ! video/x-raw,format=BGR ! appsink name=sink", WEBCAM)).unwrap();
 
     let appsink = input_pipeline
     .by_name("sink").unwrap()
@@ -34,24 +32,40 @@ fn main() -> Result<()> {
     input_pipeline.set_state(gst::State::Playing)?;
     output_pipeline.set_state(gst::State::Playing)?;
 
-    // Wait until EOS (end-of-stream) or error
-    let bus = pipeline.bus().unwrap();
-    for msg in bus.iter_timed(gst::ClockTime::NONE) {
-        match msg.view() {
-            gst::MessageView::Eos(..) => {
-                println!("Stream ended.");
-                break;
-            }
-            gst::MessageView::Error(err) => {
-                eprintln!("Error: {}", err.error());
-                break;
-            }
-            _ => {}
-        }
-    }
+
+
+    // Set caps on appsrc to follow a defined format
+    // BGR 640x480 30fps
+    let caps = gst::Caps::builder("video/x-raw")
+        .field("format", "BGR")
+        .field("width", 640)
+        .field("height", 480)
+        .field("framerate", gst::Fraction::new(30, 1))
+        .build();
+    appsrc.set_caps(Some(&caps));
+
+
+    // Implement callbacks to access frame by frame data
+    appsink.set_callbacks(
+        gst_app::AppSinkCallbacks::builder()
+            .new_sample(move |sink| {
+                let sample = sink.pull_sample().unwrap();
+                let buffer = sample.buffer().unwrap();
+
+                // Copy buffer and do ML / drawing
+                let mut processed_buffer = buffer.copy(); 
+                // ⬆️ Replace this with actual frame processing
+
+                appsrc.push_buffer(processed_buffer).unwrap();
+                Ok(gst::FlowSuccess::Ok)
+            })
+            .build(),
+    );
+
 
     // Clean up
-    pipeline.set_state(gst::State::Null)?;
+    input_pipeline.set_state(gst::State::Null)?;
+    output_pipeline.set_state(gst::State::Null)?;
     Ok(())
 }
 
